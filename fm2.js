5c1bf5e0d5e6ec0aa114a4384e55f5160360ff8b
forresttheman <96036883+forresttheman@users.noreply.github.com>
2023-01-13 18:35:34 -0800

    Add Git Ignore, static file server

--- fm2.js
+++ fm2.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+// listen on path and listen to handler
 app.get('/', function (req, res) {
   res.send('Hello World');
-})
+});
+
+// use the express package to serve a file (with the file name coming after the slash)
+app.use(express.static('public'))
 
 app.listen(3000);
