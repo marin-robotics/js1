68691f27e07373d8dfa7d59403d211025a032d49
David Moshal <dmoshal@gmail.com>
2023-01-13 18:36:28 -0800

    experiment with get-tam

--- dmfb1.js
+++ dmfb1.js
@@ -1,10 +1,10 @@
 
 const { initializeApp } = require('firebase/app');
 const { getFirestore, collection, getDocs } = require('firebase/firestore/lite');
-const x = require('firebase/database')
+const { getDatabase, ref, set, child, update, remove, push, get } = require('firebase/database')
 const admin = require('firebase-admin')
 
-console.log(x)
+//console.log({ref})
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
@@ -18,11 +18,36 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-console.log({app})
+//console.log({app})
 
-const db = getFirestore(app);
-console.log({db})
+//const db = getFirestore(app);
+const db = getDatabase(app)
+const reference = ref(db);
 
+var leaderboard = ref(db, 'Leaderboard');
 
+console.log([leaderboard])
+
+var games = get(child(reference, 'TotalGamesPlayed')).then((snapshot) => {
+    var temp = snapshot.val();
+    set(game, temp+1);
+  });
+
+//get(child(reference, 'Leaderboard')).then((snapshot) => {
+//    console.log(snapshot)
+    // var temp = snapshot.val();
+    // for(let i = 0; i < temp.length; i ++) {
+    //    const name = temp[i] ? temp[i].name : false;
+    //    console.log(name)
+    // //   if(!name || name == "" || name == " " || temp[i].name.length < 3 || temp[i].name.length > 20) {
+    // //     temp.splice(i, 1);
+    // //     temp.push({name: '', score: 0, id: 'aaa000'});
+    // //   }
+    // }
+    // for(let i = 0; i < 50; i ++) {
+    //     temp.push({name: '', score: 0, id: 'aaa000'});
+    // }
+    // set(leaderboard, temp);
+ // });
 
 
